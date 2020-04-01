3248d5b1a751e8e468949beca1ac915159271545
Helter Pinheiro <helterpinheiro@gmail.com>
2020-04-01 01:32:10 -0300

    [API FRONT] Finalizando a conexão entre API e FRONT

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiLogIn } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom'
 
 import './style.css';
+import api from '../../services/api'
 import heroesImg from '../../assets/heroes.png';
 import logo from '../../assets/logo.svg';
 
 export default function Logon(){
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+    async function handleLogin(e) {
+        e.preventDefault();
+        try {
+            console.log(id);
+            const response = await api.post('session', {id});
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile')
+        } catch {
+            alert('Falha no login, tente novamente')
+        }
+    }
+
     return (
         <div className = "logonContainer">
         <section className = "form">
             <img src = {logo} alt = "Be the Hero"/>
-            <form>
+            <form onSubmit = {handleLogin}>
                 <h1>Faça seu Logon</h1>
-                <input placeholder = "Sua ID" />
+                <input placeholder = "Sua ID" value = {id} onChange = {e => setId(e.target.value)} />
                 <button className = "button" type = "submit">Entrar</button>
                 <Link className = "back-link" to = "/register">
                     <FiLogIn size = {16} color = "#E0204" />
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api'
 
 import { FiArrowLeft } from 'react-icons/fi';
 import './style.css';
 
 export default function NewIncident() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('')
+
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            value,
+        }
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+            history.push('/profile');
+        } catch {
+            alert('Erro ao cadastrar caso, tente novamente')
+        }
+    }
+
     return (
         <div className = "new-incident-container">
             <div className = "content">
@@ -18,10 +46,10 @@ export default function NewIncident() {
                         Voltar pra home
                     </Link>
                 </section>
-                <form onSubmit = "">
-                    <input placeholder = "Titulo do caso" value = "" onChange = ""></input>
-                    <textarea placeholder = "Descrição" value = "" onChange = ""></textarea>
-                    <input placeholder = "Valor em reais" value = "" onChange = ""></input>
+                <form onSubmit = {handleSubmit}>
+                    <input placeholder = "Titulo do caso" value = { title } onChange = {e => setTitle(e.target.value)} ></input>
+                    <textarea placeholder = "Descrição" value = { description } onChange = {e => setDescription(e.target.value)}></textarea>
+                    <input placeholder = "Valor em reais" value = { value } onChange = {e => setValue(e.target.value)}></input>
                     <button className = "button" type = "submit">Registrar</button>
                 </form>
             </div>
